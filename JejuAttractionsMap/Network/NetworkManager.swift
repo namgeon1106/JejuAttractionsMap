35d10e5e10e304dd159f5fa8c0f0ed1fec6329ae
namgeon1106 <namgeon1106@naver.com>
2023-03-07 17:19:47 +0900

    feat: 관광지 정보 fetch가 정상적으로 된 케이스 구현

--- JejuAttractionsMap/Network/NetworkManager.swift
+++ JejuAttractionsMap/Network/NetworkManager.swift
@@ -9,13 +9,25 @@ import Foundation
 
 class NetworkManager {
     let session: URLSessionProtocol
-    let attractionsCount = 1047
+    private let attractionsCount = 1047
+    private let serviceKey = "P6%2BlyELthAZGU%2BPOAS5mE3%2BqJX78QgOALbnBIdeZZOYnNjpa5TcB7OyWIODvx7dN1VZkMsDvEX554ddUBuYqhg%3D%3D"
+    
+    private var attractionsURLString: String {
+        "https://apis.data.go.kr/6500000/jjtb/locinfo?serviceKey=\(serviceKey)&pageNo=1&numOfRows=\(attractionsCount)"
+    }
     
     init(session: URLSessionProtocol = URLSession.shared) {
         self.session = session
     }
     
     func fetchAllAttractions() async throws -> [Attraction] {
+        let url = URL(string: attractionsURLString)!
+        let (data, response) = try await session.data(for: URLRequest(url: url))
+        
+        if let attractionsResponse = try? JSONDecoder().decode(AttractionsResponse.self, from: data) {
+            return attractionsResponse.data
+        }
+        
         return []
     }
 }
