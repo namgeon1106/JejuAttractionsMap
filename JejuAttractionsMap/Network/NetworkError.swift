ac018065e87f5de74c38e58a5162f59b510debe4
namgeon1106 <namgeon1106@naver.com>
2023-03-07 17:36:47 +0900

    feat: NetworkError 추가

---
+++ JejuAttractionsMap/Network/NetworkError.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkError.swift
+//  JejuAttractionsMap
+//
+//  Created by 김남건 on 2023/03/07.
+//
+
+import Foundation
+
+enum NetworkError {
+    case serviceExpired, serviceAccessDenied, requestExceeded, unknown
+}
