aba43c9af6db50dd828dd7a2da538f74d17b3202
namgeon1106 <namgeon1106@naver.com>
2023-03-15 19:50:48 +0900

    test: fetchAllAttractions()에 대한 integration test 작성

--- JejuAttractionsMapTests/Tests/NetworkManagerIntegrationTests.swift
+++ JejuAttractionsMapTests/Tests/NetworkManagerIntegrationTests.swift
@@ -9,8 +9,23 @@ import XCTest
 @testable import JejuAttractionsMap
 
 final class NetworkManagerIntegrationTests: XCTestCase {
+    let sut = NetworkManager(session: URLSession.shared)
 
-    func testExample() throws {
+    func testFetchAttractions() {
+        let expectation = expectation(description: "Task must be executed.")
+        
+        Task {
+            do {
+                let attractions = try await sut.fetchAllAttractions()
+                XCTAssertEqual(attractions.count, 1047)
+            } catch {
+                print("error: \(error)")
+                XCTFail()
+            }
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 3)
     }
 
 }
