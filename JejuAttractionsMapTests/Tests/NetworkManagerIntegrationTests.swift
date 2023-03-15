da816e1b6c5cd6ad95fd0ff01ecb487c6fdf7651
namgeon1106 <namgeon1106@naver.com>
2023-03-15 19:43:09 +0900

    test: integration test를 위한 테스트 생성 및 그룹화

---
+++ JejuAttractionsMapTests/Tests/NetworkManagerIntegrationTests.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkManagerIntegrationTests.swift
+//  JejuAttractionsMapTests
+//
+//  Created by 김남건 on 2023/03/15.
+//
+
+import XCTest
+@testable import JejuAttractionsMap
+
+final class NetworkManagerIntegrationTests: XCTestCase {
+
+    func testExample() throws {
+    }
+
+}
