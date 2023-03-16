15b3103381ac55fefea23a6b16ba2c7dd60a9c0d
namgeon1106 <namgeon1106@naver.com>
2023-03-16 21:17:08 +0900

    feat: UI layout 설정

--- JejuAttractionsMap/ViewController.swift
+++ JejuAttractionsMap/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import NMapsMap
 
 class ViewController: UIViewController {
+    let searchBar = UISearchBar()
+    let searchCancelButton = {
+        let button = UIButton()
+        button.setTitle("", for: .normal)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.widthAnchor.constraint(equalToConstant: 45),
+            button.heightAnchor.constraint(equalToConstant: 30)
+        ])
+        
+        return button
+    }()
+    
+    let tableView = UITableView()
+    let mapView = NMFNaverMapView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setLayout()
     }
 
-
+    func setLayout() {
+        [searchBar, searchCancelButton, tableView, mapView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: searchCancelButton.leadingAnchor),
+            searchBar.heightAnchor.constraint(equalToConstant: 30),
+            searchCancelButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchCancelButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            mapView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
+        tableView.isHidden = true
+        searchCancelButton.isHidden = true
+    }
 }
 
