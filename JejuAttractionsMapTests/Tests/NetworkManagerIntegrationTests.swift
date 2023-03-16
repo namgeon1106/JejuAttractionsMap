d4b261e2901a72164d8ad624146c44a5a812539e
namgeon1106 <namgeon1106@naver.com>
2023-03-16 13:32:57 +0900

    test: fetchImage에 대한 테스트 작성

--- JejuAttractionsMapTests/Tests/NetworkManagerIntegrationTests.swift
+++ JejuAttractionsMapTests/Tests/NetworkManagerIntegrationTests.swift
@@ -27,5 +27,26 @@ final class NetworkManagerIntegrationTests: XCTestCase {
         
         wait(for: [expectation], timeout: 3)
     }
-
+    
+    func testFetchImage_whenUsedCorrectUrl_returnsUIImage() {
+        let data = try! Data.fromFile(fileName: "가릿당_관광지안내_001-1", format: "jpg")
+        let expectedImage = UIImage(data: data)
+        
+        let expectation = expectation(description: "Task must be executed.")
+        
+        Task {
+            do {
+                let image = try await sut.fetchImage(from: "http://api.brandcontents.or.kr/jejuImage/%EA%B0%80%EB%A6%BF%EB%8B%B9_%EA%B4%80%EA%B4%91%EC%A7%80%EC%95%88%EB%82%B4_001-1.jpg")
+                
+                XCTAssertEqual(image.jpegData(compressionQuality: 1), expectedImage?.jpegData(compressionQuality: 1))
+            } catch {
+                print(error)
+                XCTFail()
+            }
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 3)
+    }
 }
