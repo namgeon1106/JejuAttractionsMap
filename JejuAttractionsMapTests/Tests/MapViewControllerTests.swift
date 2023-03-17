6848e1fba9aa8dea9801d68176e15b97ff96d10c
namgeon1106 <namgeon1106@naver.com>
2023-03-17 17:25:17 +0900

    test: MapViewControllerTests에서 초기 상태와 startSearch() 이후 상태에 대한 테스트 작성

---
+++ JejuAttractionsMapTests/Tests/MapViewControllerTests.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewControllerTests.swift
+//  JejuAttractionsMapTests
+//
+//  Created by 김남건 on 2023/03/16.
+//
+
+import XCTest
+@testable import JejuAttractionsMap
+
+final class MapViewControllerTests: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
