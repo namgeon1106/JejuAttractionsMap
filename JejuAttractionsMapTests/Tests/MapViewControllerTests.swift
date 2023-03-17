5d19129958ab8dc304cd43c1689161634d3ec8e0
namgeon1106 <namgeon1106@naver.com>
2023-03-17 23:23:09 +0900

    test: 검색 텍스트 입력 시 view controller 상태에 대한 테스트 추가

--- JejuAttractionsMapTests/Tests/MapViewControllerTests.swift
+++ JejuAttractionsMapTests/Tests/MapViewControllerTests.swift
@@ -24,6 +24,13 @@ final class MapViewControllerTests: XCTestCase {
     
     override func setUpWithError() throws {
         sut.viewModel.initializeState()
+        
+        let expectation = expectation(description: "Wait for debounce operator.")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 1)
     }
 
     func testViewController_whenInitialized_updatesUI() {
@@ -56,5 +63,32 @@ final class MapViewControllerTests: XCTestCase {
         XCTAssertEqual(sut.searchBar.text, "")
         XCTAssertEqual(sut.tableView.isHidden, true)
         XCTAssertEqual(sut.mapView.isHidden, false)
+    }
+    
+    func testViewController_whenSearchForText_updatesUI() {
+        // given
+        sut.viewModel.startSearch()
+        
+        // when
+        sut.viewModel.searchFor("1112")
+        
+        // then
+        XCTAssertEqual(sut.searchBar.text, "1112")
+        XCTAssertEqual(sut.activityIndicator.isHidden, false)
+        XCTAssertEqual(sut.activityIndicator.isAnimating, true)
+        
+        let expectation = expectation(description: "Wait for debounce publisher.")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            XCTAssertEqual(self.sut.activityIndicator.isHidden, true)
+            XCTAssertEqual(self.sut.activityIndicator.isAnimating, false)
+            
+            let expectedCount = self.sut.viewModel.attractions.filter { attraction in
+                attraction.name.contains("1112")
+            }.count
+            
+            XCTAssertEqual(self.sut.tableView.numberOfRows(inSection: 0), expectedCount)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
     }
 }
