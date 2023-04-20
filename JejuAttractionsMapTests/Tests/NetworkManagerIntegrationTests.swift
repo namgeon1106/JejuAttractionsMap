d088f0a5773cc18947e8461eef0113d85d027bfc
namgeon1106 <namgeon1106@naver.com>
2023-04-20 21:39:54 +0900

    test: fetchImageUrlString에 대한 통합 테스트 추가

--- JejuAttractionsMapTests/Tests/NetworkManagerIntegrationTests.swift
+++ JejuAttractionsMapTests/Tests/NetworkManagerIntegrationTests.swift
@@ -67,4 +67,21 @@ final class NetworkManagerIntegrationTests: XCTestCase {
         
         wait(for: [expectation], timeout: 3)
     }
+    
+    func testFetchImageUrlString() {
+        let expectation = expectation(description: "Task must be executed.")
+        
+        Task {
+            do {
+                let result = try await sut.fetchImageURLString(for: "성산일출봉")
+                XCTAssertEqual(result, "https://search.pstatic.net/sunny/?type=b150&src=https://resource.tranggle.com/board_v3/story/2022/20220531/231404_3a11dd9c98ea1f6288b3d1700430626dda41293d_960x.jpg")
+            } catch {
+                print("error: \(error)")
+                XCTFail()
+            }
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 3)
+    }
 }
